c93610e1a41ee3cec5133e558f3fc7f1546084b5
Jens Bleumer <jens.bleumer@gmail.com>
2023-05-08 12:32:41 +0200

    jbl(): add CORS to the project

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { NODE_ENV } from './common/constants';
 import { setupSwagger } from './utils';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
