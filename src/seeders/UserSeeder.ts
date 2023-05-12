00c52db483e39b569b1650839839525660fba37a
Jens Bleumer <jens.bleumer@gmail.com>
2023-05-12 11:49:30 +0200

    jbl(): update seeder

--- src/seeders/UserSeeder.ts
+++ src/seeders/UserSeeder.ts
@@ -8,21 +8,25 @@ import { Role } from 'src/api/users/users.interface';
 export class UserSeeder extends Seeder {
   /* eslint-disable @typescript-eslint/no-unused-vars */
   async run(em: EntityManager): Promise<void> {
-    const admin = em.upsert(User, {
-      firstName: 'Admin',
-      lastName: 'Api',
-      userName: 'admin',
-      email: 'admin@timebooking.com',
-      role: Role.ADMIN,
-      passwordHash: await bcrypt.hash('Abcd!1234', 10),
-      verified: new Date(Date.UTC(2000, 0, 1)),
-      acceptTerms: true,
-    });
+    const countAdmin = await em.count(User, { role: Role.ADMIN });
+
+    if (countAdmin < 1) {
+      const admin = em.create(User, {
+        firstName: 'Admin',
+        lastName: 'Api',
+        userName: 'admin',
+        email: 'admin@timebooking.com',
+        role: Role.ADMIN,
+        passwordHash: await bcrypt.hash('Abcd!1234', 10),
+        verified: new Date(Date.UTC(2000, 0, 1)),
+        acceptTerms: true,
+      })
+    }
 
     let n =
       process.env.SEED_VISITORS === undefined ? 10 : +process.env.SEED_VISITORS;
-    const [_, count] = await em.findAndCount(User, { role: Role.VISITOR });
-    n = n - Math.min(count, n);
+    const countVisitors = await em.count(User, { role: Role.VISITOR });
+    n = n - Math.min(countVisitors, n);
     const users = await new UsersFactory(em).create(n, {
       verified: new Date(Date.UTC(2000, 0, 1)),
       acceptTerms: true,
